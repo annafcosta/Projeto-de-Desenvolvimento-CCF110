7be59ffb4ce059837346530270e6593517311f06
Anna Luísa <annalf1401@gmail.com>
2023-09-17 20:00:15 -0300

    Implementação das entradas e leituras dos produtos.

--- sistema_de_vendas.c
+++ sistema_de_vendas.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
-int main(){
+int main()
+{
+    int max_produto;
 
-    return  0;
+    printf("Insira a quantidade máxima de produtos produzidos nesse mês: ");
+    scanf("%d", &max_produto);
+
+    // Declarando os vetores para armazenar as informações dos produtos:
+    char nome_produto[max_produto][50];
+    float valor_produto[max_produto];
+    int qtd_produto[max_produto];
+    int codigo_produto[max_produto];
+    int num_produtos = 0;
+
+    for (int i = 0; i < max_produto; ++i)
+    {
+        if (num_produtos < max_produto)
+        {
+            // Código do produto é automaticamente atribuído:
+            int codigo = num_produtos + 1;
+
+            printf("Informe o nome do produto: ");
+            scanf("%s", nome_produto[num_produtos]);
+            printf("Informe o preço do produto: ");
+            scanf("%f", &valor_produto[num_produtos]);
+            printf("Informe a quantidade em estoque: ");
+            scanf("%d", &qtd_produto[num_produtos]);
+
+            // Atribuir o código do produto:
+
+            codigo_produto[num_produtos] = codigo;
+
+            num_produtos++;
+            printf("Produto adicionado com sucesso! Código do produto: %d\n", codigo);
+        } else
+        {
+            printf("Limite de produtos atingido!\n");
+        }
+    }
+
+    // REGISTRO DE VENDAS:
+
+    int max_vendas;
+
+    printf("Insira a quantidade máxima de produtos a serem vendidos nesse mês: ");
+    scanf("%d", &max_vendas);
+
+    // Declarando as variáveis para registrar as vendas:
+
+    int nome_vendidos[max_vendas];
+    int qtd_vendidas[max_vendas];
+    float totalVenda[max_vendas];
+    int num_vendas = 0;
+
+
+    return 0;
 }
