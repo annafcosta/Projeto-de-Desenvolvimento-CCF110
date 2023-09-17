8b640e26aceea6c74f3bcafc6b86a417446bf5e8
Hellen Carolyna <hellenmagalhaes500@gmail.com>
2023-09-17 19:45:40 -0300

    Adicionando menu inicial

--- sistema_de_vendas.c
+++ sistema_de_vendas.c
@@ -2,7 +2,33 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main(){
+int main()
+{
+    int opcao;
+
+    printf("\n\n~~~~~~~~~~~~~~~~~~~~-DELICIAS DA HELLEN-~~~~~~~~~~~~~~~~~~~~\n\n");
+    do
+    {
+        printf("\n\t1 - Cadastrar novo produto no estoque");
+        printf("\n\t2 - Visualizar produtos no estoque");
+        printf("\n\t3 - Cadastrar nova venda");
+        printf("\n\t4 - Listar vendas");
+        printf("\n\t0 - Sair");
+        printf("\n\n Digite a opcao desejada:");
+        scanf("%d", &opcao);
+
+        switch(opcao)
+        {
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+            case 0: break;
+            default:
+                printf("Escolha uma opcao valida!!! ");
+        }
+    }while(opcao != 0);
+
 
     return  0;
 }
