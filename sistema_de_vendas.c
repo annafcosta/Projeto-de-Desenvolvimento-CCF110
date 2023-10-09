b446394126ec5dc8d10e580e34caef4705e582d2
Anna Luísa <annalf1401@gmail.com>
2023-10-08 23:30:25 -0300

    Implementação de verificações para invalidar números negativos.

--- sistema_de_vendas.c
+++ sistema_de_vendas.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+// 
 
 int main()
 {
+
+//    int opcao;
+//
+//    printf("\n\n~~~~~~~~~~~~~~~~~~~~-DELÍCIAS DA HELLEN-~~~~~~~~~~~~~~~~~~~~\n\n");
+//    do
+//    {
+//        printf("\n\t1 - Cadastrar novo produto no estoque");
+//        printf("\n\t2 - Visualizar produtos no estoque");
+//        printf("\n\t3 - Cadastrar nova venda");
+//        printf("\n\t4 - Listar vendas");
+//        printf("\n\t0 - Sair");
+//        printf("\n\n Digite a opção desejada: ");
+//        scanf("%d", &opcao);
+//
+//        switch(opcao)
+//        {
+//            case 1: break;
+//            case 2: break;
+//            case 3: break;
+//            case 4: break;
+//            case 0: break;
+//            default:
+//                printf("Escolha uma opcao valida!!! ");
+//        }
+//    }while(opcao != 0);
+
     // CADASTRO DOS PRODUTOS
 
     int max_produto;
@@ -24,31 +51,6 @@ int main()
     float valor_produto[max_produto];
     int quantidade_produto[max_produto];
     int num_produtos = 0;
-
-//    int opcao;
-//
-//    printf("\n\n~~~~~~~~~~~~~~~~~~~~-DELICIAS DA HELLEN-~~~~~~~~~~~~~~~~~~~~\n\n");
-//    do
-//    {
-//        printf("\n\t1 - Cadastrar novo produto no estoque");
-//        printf("\n\t2 - Visualizar produtos no estoque");
-//        printf("\n\t3 - Cadastrar nova venda");
-//        printf("\n\t4 - Listar vendas");
-//        printf("\n\t0 - Sair");
-//        printf("\n\n Digite a opcao desejada:");
-//        scanf("%d", &opcao);
-//
-//        switch(opcao)
-//        {
-//            case 1: break;
-//            case 2: break;
-//            case 3: break;
-//            case 4: break;
-//            case 0: break;
-//            default:
-//                printf("Escolha uma opcao valida!!! ");
-//        }
-//    }while(opcao != 0);
 
     for (int i = 0; i < max_produto; ++i)
     {
