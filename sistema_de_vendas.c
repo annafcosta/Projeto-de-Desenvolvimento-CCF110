b0dc8542ef28f40f52908de9068896a6eb2fef4b
Anna Luísa <annalf1401@gmail.com>
2023-10-26 12:10:24 -0300

    Update sistema_de_vendas.c
    Implementação da função "Registro de Vendas"

--- sistema_de_vendas.c
+++ sistema_de_vendas.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
+int max_produto = 2;
+
 int main()
 {
     int opcao;
 
     // Declarando as variáveis para os produtos fora do loop:
-    int max_produto = 1;
     char nome_produto[max_produto][50];
     float valor_produto[max_produto];
+    int codigo_produto[max_produto];
     int quantidade_produto[max_produto];
     int num_produtos = 0;
 
@@ -36,46 +38,59 @@ int main()
         printf("\n\n Digite a opção desejada: ");
         scanf("%d", &opcao);
 
-
         // CADASTRO DOS PRODUTOS
         if (opcao == 1)
         {
             // NOME DO PRODUTO:
             for (int i = 0; i < max_produto; i++)
             {
-                printf("\nInforme o nome do produto: ");
+                printf("\nInforme o nome do produto %d: ", i + 1);
                 getchar(); // Consumir a nova linha pendente
-                fgets(nome_produto[num_produtos], sizeof(nome_produto[i]), stdin);
-                nome_produto[num_produtos][strcspn(nome_produto[i], "\n")] = '\0';
+                fgets(nome_produto[i], sizeof(nome_produto[i]) + 1, stdin);
+                nome_produto[i][strcspn(nome_produto[i], "\n")] = '\0';
+                printf("%s \n", nome_produto[i]);
             }
+
+            // CÓDIGO DO PRODUTO:
+            do
+            {
+                for (int i = 0; i < max_produto; i++)
+                {
+                    printf("Informe o código do produto %d: ", i + 1);
+                    scanf("%d", &codigo_produto[i]);
+
+                    if (codigo_produto[i] < 0) // Verificação para números negativos;
+                    {
+                        printf("O código do produto não pode ser negativo. Tente novamente.\n");
+                    }
+                }
+
+            } while (codigo_produto[num_produtos] < 0);
 
             // VALOR DO PRODUTO:
             do
             {
                 for (int i = 0; i < max_produto; i++)
                 {
-
-                    printf("Informe o valor do produto: ");
+                    printf("Informe o valor do produto %d: ", i + 1);
                     scanf("%f", &valor_produto[i]);
 
-                    if (valor_produto[i] < 0) // Verificação para números negativos;
+                    if (valor_produto[i] < 0)
                     {
                         printf("O valor do produto não pode ser negativo. Tente novamente.\n");
                     }
-
-
                 }
-
             } while (valor_produto[num_produtos] < 0);
 
-            do // QUANTIDADE DE PRODUTOS
+            // QUANTIDADE DE PRODUTOS
+            do
             {
                 for (int i = 0; i < max_produto; i++)
                 {
-                    printf("Informe a quantidade em estoque: ");
+                    printf("Informe a quantidade em estoque do produto %d: ", i + 1);
                     scanf("%d", &quantidade_produto[i]);
 
-                    if (quantidade_produto[i] < 0) // Verificação para números negativos;
+                    if (quantidade_produto[i] < 0)
                     {
                         printf("A quantidade do produto não pode ser negativa. Tente novamente.\n");
                     }
@@ -87,19 +102,70 @@ int main()
             printf("\033[0;33mProduto adicionado com sucesso!\n");
             printf("\033[0m");
 
-        } else if (opcao == 2) // VISUALIZAÇÃO DE PRODUTOS
+        }
+
+            // VISUALIZAÇÃO DE PRODUTOS
+        else if (opcao == 2)
         {
-            for (int i = 0; i < num_produtos; ++i)
+            for (int i = 0; i < max_produto; ++i)
             {
                 printf("\n\033[1;32mPRODUTO %d\033[0m:\n", i + 1);
                 printf("\033[35mNOME: %s\033[0m\n", nome_produto[i]);
-                printf("\033[34mVALOR: R$%.2f\033[0m\n", valor_produto[i]);
+                printf("\033[34mCÓDIGO: %d \033[0m\n", codigo_produto[i]);
+                printf("\033[31mVALOR: R$ %.2f \033[0m\n", valor_produto[i]);
                 printf("\033[38;5;208mQUANTIDADE: %d\033[0m\n", quantidade_produto[i]);
                 printf("\n");
             }
-        } else if (opcao == 3)
+        }
+
+            // CADASTRO DE VENDAS:
+        else if (opcao == 3)
         {
-            // ... código para o caso 3 ...
+            int codigo_venda;
+            int quantidade_vendida;
+
+            printf("\nInforme o código do produto vendido: ");
+            scanf("%d", &codigo_venda);
+
+            // Procurar o código do produto no array
+            int produto_encontrado = -1;
+            for (int i = 0; i < num_produtos; i++)
+            {
+                if (codigo_produto[i] == codigo_venda)
+                {
+                    produto_encontrado = i;
+                    break;
+                }
+            }
+
+            if (produto_encontrado != -1)
+            {
+                printf("Informe a quantidade vendida: ");
+                scanf("%d", &quantidade_vendida);
+
+                // Verificar se há quantidade suficiente em estoque
+                if (quantidade_vendida <= quantidade_produto[produto_encontrado])
+                {
+                    // Atualizar a quantidade em estoque
+                    quantidade_produto[produto_encontrado] -= quantidade_vendida;
+
+                    // Calcular o valor total da venda
+                    float valor_total_venda = valor_produto[produto_encontrado] * quantidade_vendida;
+
+                    // Imprimir o valor total da venda
+                    printf("\033[0;33mVenda registrada com sucesso!\n");
+                    printf("\033[0;33mValor da venda: R$ %.2f\n", valor_total_venda);
+                    printf("\033[0m");
+                } else
+                {
+                    printf("\033[31mQuantidade insuficiente em estoque para realizar a venda.\n");
+                    printf("\033[0m");
+                }
+            } else
+            {
+                printf("\033[31mProduto não encontrado. Verifique o código do produto.\n");
+                printf("\033[0m");
+            }
         } else if (opcao == 4)
         {
             // ... código para o caso 4 ...
